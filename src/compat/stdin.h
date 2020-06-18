3af5933e37c35b159411192ab3a149e7a1455d25
Unify <unify@eleccoin.org>
2020-06-19 00:06:52 +0800

    eleccoin-cli: -stdinwalletpassphrase and non-echo stdin passwords
    This PR
    
    adds -stdinwalletpassphrase for use with walletpasshprase(change)
    adds no-echo for passwords (-stdinrpcpass and above)
    It may not be ideal, but it's better than having to clear the screen whenever you unlock the wallet.

---
+++ src/compat/stdin.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_COMPAT_STDIN_H
+#define ELECCOIN_COMPAT_STDIN_H
+
+struct NoechoInst {
+    NoechoInst();
+    ~NoechoInst();
+};
+
+#define NO_STDIN_ECHO() NoechoInst _no_echo
+
+bool StdinTerminal();
+bool StdinReady();
+
+#endif // ELECCOIN_COMPAT_STDIN_H
