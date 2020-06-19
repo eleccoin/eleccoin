330aca6b238d25cf0daeeb1f30f67c4003e7e580
Unify <unify@eleccoin.org>
2020-06-20 00:19:20 +0800

    Add util at test

---
+++ src/test/util/blockfilter.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_TEST_UTIL_BLOCKFILTER_H
+#define ELECCOIN_TEST_UTIL_BLOCKFILTER_H
+
+#include <blockfilter.h>
+class CBlockIndex;
+
+bool ComputeFilter(BlockFilterType filter_type, const CBlockIndex* block_index, BlockFilter& filter);
+
+#endif // ELECCOIN_TEST_UTIL_BLOCKFILTER_H
