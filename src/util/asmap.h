6ffa423be59bcbf4752f7ed73fb5dfd34ede7a52
Unify <unify@eleccoin.org>
2020-06-20 17:35:12 +0800

    Add new modules

---
+++ src/util/asmap.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_UTIL_ASMAP_H
+#define ELECCOIN_UTIL_ASMAP_H
+
+uint32_t Interpret(const std::vector<bool> &asmap, const std::vector<bool> &ip);
+
+#endif // ELECCOIN_UTIL_ASMAP_H
---
+++ src/util/macros.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_UTIL_MACROS_H
+#define ELECCOIN_UTIL_MACROS_H
+
+#define PASTE(x, y) x ## y
+#define PASTE2(x, y) PASTE(x, y)
+
+#endif // ELECCOIN_UTIL_MACROS_H
