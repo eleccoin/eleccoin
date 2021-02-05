249ac2918129a49122a1bc6d151916bfe7542408
Unify <unify@eleccoin.org>
2021-02-05 19:45:29 +0800

    Upgrade check.

--- src/util/check.h
+++ src/util/check.h
@@ -1,9 +1,13 @@
-// Copyright (c) 2019 The Eleccoin Core developers
+// Copyright (c) 2020-2021 The Eleccoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef ELECCOIN_UTIL_CHECK_H
 #define ELECCOIN_UTIL_CHECK_H
+
+#if defined(HAVE_CONFIG_H)
+#include <config/eleccoin-config.h>
+#endif
 
 #include <tinyformat.h>
 
@@ -21,7 +25,7 @@ class NonFatalCheckError : public std::runtime_error
  * - where the condition is assumed to be true, not for error handling or validating user input
  * - where a failure to fulfill the condition is recoverable and does not abort the program
  *
- * For example in RPC code, where it is undersirable to crash the whole program, this can be generally used to replace
+ * For example in RPC code, where it is undesirable to crash the whole program, this can be generally used to replace
  * asserts or recoverable logic errors. A NonFatalCheckError in RPC code is caught and passed as a string to the RPC
  * caller, which can then report the issue to the developers.
  */
@@ -37,5 +41,19 @@ class NonFatalCheckError : public std::runtime_error
                     PACKAGE_BUGREPORT));                          \
         }                                                         \
     } while (false)
+
+#if defined(NDEBUG)
+#error "Cannot compile without assertions!"
+#endif
+
+/** Helper for Assert(). TODO remove in C++14 and replace `decltype(get_pure_r_value(val))` with `T` (templated lambda) */
+template <typename T>
+T get_pure_r_value(T&& val)
+{
+    return std::forward<T>(val);
+}
+
+/** Identity function. Abort if the value compares equal to zero */
+#define Assert(val) [&]() -> decltype(get_pure_r_value(val)) { auto&& check = (val); assert(#val && check); return std::forward<decltype(get_pure_r_value(val))>(check); }()
 
 #endif // ELECCOIN_UTIL_CHECK_H
