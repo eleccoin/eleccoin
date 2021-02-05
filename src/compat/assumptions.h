6e60aa47f422cc1738e6414ed8b14f596eaf2c30
Unify <unify@eleccoin.org>
2021-02-05 22:03:27 +0800

    Upgrade common.

--- src/compat/assumptions.h
+++ src/compat/assumptions.h
@@ -1,5 +1,4 @@
-// Copyright (c) 2009-2010
-// Copyright (c) 2009-2019 The Eleccoin Core developers
+// Copyright (c) 2021 The Eleccoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -50,6 +49,7 @@ static_assert(sizeof(double) == 8, "64-bit double assumed");
 //             code.
 static_assert(sizeof(short) == 2, "16-bit short assumed");
 static_assert(sizeof(int) == 4, "32-bit int assumed");
+static_assert(sizeof(unsigned) == 4, "32-bit unsigned assumed");
 
 // Assumption: We assume size_t to be 32-bit or 64-bit.
 // Example(s): size_t assumed to be at least 32-bit in ecdsa_signature_parse_der_lax(...).
