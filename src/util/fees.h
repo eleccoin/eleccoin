d6e6288c8fdbed4f744b677857a460af38b4444e
Unify <unify@eleccoin.org>
2021-02-05 19:48:32 +0800

    Upgrade fees util.

--- src/util/fees.h
+++ src/util/fees.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 The Eleccoin Core developers
+// Copyright (c) 2020-2021 The Eleccoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef ELECCOIN_UTIL_FEES_H
@@ -11,5 +11,7 @@ enum class FeeReason;
 
 bool FeeModeFromString(const std::string& mode_string, FeeEstimateMode& fee_estimate_mode);
 std::string StringForFeeReason(FeeReason reason);
+std::string FeeModes(const std::string& delimiter);
+const std::string InvalidEstimateModeErrorMessage();
 
 #endif // ELECCOIN_UTIL_FEES_H
