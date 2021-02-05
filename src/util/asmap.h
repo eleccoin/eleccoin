f5319d2f5f96cbf2fbc01ec97ecb7ab739ace9f7
Unify <unify@eleccoin.org>
2021-02-05 19:42:45 +0800

    Upgrade asmap util.

--- src/util/asmap.h
+++ src/util/asmap.h
@@ -1,10 +1,15 @@
-// Copyright (c) 2019 The Eleccoin Core developers
+// Copyright (c) 2020-2021 The Eleccoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef ELECCOIN_UTIL_ASMAP_H
 #define ELECCOIN_UTIL_ASMAP_H
 
+#include <stdint.h>
+#include <vector>
+
 uint32_t Interpret(const std::vector<bool> &asmap, const std::vector<bool> &ip);
+
+bool SanityCheckASMap(const std::vector<bool>& asmap, int bits);
 
 #endif // ELECCOIN_UTIL_ASMAP_H
