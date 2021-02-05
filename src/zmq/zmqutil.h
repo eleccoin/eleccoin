a8a38a06e91d3ddf6fb12a010979a8b5f6772dd8
Unify <unify@eleccoin.org>
2021-02-05 21:36:06 +0800

    Add zmqutil.

---
+++ src/zmq/zmqutil.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_ZMQ_ZMQUTIL_H
+#define ELECCOIN_ZMQ_ZMQUTIL_H
+
+void zmqError(const char* str);
+
+#endif // ELECCOIN_ZMQ_ZMQUTIL_H
