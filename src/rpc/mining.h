5e25a83d780bd3dd93c2ce5f4354d70a1dd0152a
Unify <unify@eleccoin.org>
2021-02-05 22:59:30 +0800

    Upgrade secp256k1.

---
+++ src/rpc/mining.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_RPC_MINING_H
+#define ELECCOIN_RPC_MINING_H
+
+/** Default max iterations to try in RPC generatetodescriptor, generatetoaddress, and generateblock. */
+static const uint64_t DEFAULT_MAX_TRIES{1000000};
+
+#endif // ELECCOIN_RPC_MINING_H
---
+++ src/secp256k1/SECURITY.md
@@ -0,0 +1,3 @@
+# Security Policy
+
+You can import a key by running the following command with that individual’s fingerprint: `gpg --recv-keys "<fingerprint>"` Ensure that you put quotes around fingerprints containing spaces.
