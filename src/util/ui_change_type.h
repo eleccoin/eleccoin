dc4cc9f23dfa4cc5b5d06fc58020f32d5129e2bb
Unify <unify@eleccoin.org>
2021-02-05 16:39:08 +0800

    Add new util.

---
+++ src/util/ui_change_type.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_UTIL_UI_CHANGE_TYPE_H
+#define ELECCOIN_UTIL_UI_CHANGE_TYPE_H
+
+/** General change type (added, updated, removed). */
+enum ChangeType {
+    CT_NEW,
+    CT_UPDATED,
+    CT_DELETED
+};
+
+#endif // ELECCOIN_UTIL_UI_CHANGE_TYPE_H
