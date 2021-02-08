44ad079a42e7fb590fbb3e625ef43b23605c4c0a
Rojar Smith <rojarsmith@gmail.com>
2021-02-08 10:08:25 +0800

    Add a lots of Fuzzbuzz.

--- src/test/fuzz/hex.cpp
+++ src/test/fuzz/hex.cpp
@@ -1,10 +1,10 @@
-// Copyright (c) 2019 The Eleccoin Core developers
+// Copyright (c) 2020-2021 The Eleccoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <core_io.h>
-#include <pubkey.h>
 #include <primitives/block.h>
+#include <pubkey.h>
 #include <rpc/util.h>
 #include <test/fuzz/fuzz.h>
 #include <uint256.h>
@@ -16,7 +16,8 @@
 #include <string>
 #include <vector>
 
-void initialize() {
+void initialize()
+{
     static const ECCVerifyHandle verify_handle;
 }
 
