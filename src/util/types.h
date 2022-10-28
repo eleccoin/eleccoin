4fed340acfbc9df06000c366713e66fffe5aba31
Unify <unify@eleccoin.org>
2022-10-28 13:56:57 +0800

    Add types

---
+++ src/util/types.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 The Eleccoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECCOIN_UTIL_TYPES_H
+#define ELECCOIN_UTIL_TYPES_H
+
+template <class>
+inline constexpr bool ALWAYS_FALSE{false};
+
+#endif // ELECCOIN_UTIL_TYPES_H
