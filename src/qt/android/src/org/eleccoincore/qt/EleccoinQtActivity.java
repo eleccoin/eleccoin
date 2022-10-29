337f20a32812b55bb9415bab694fe15b29bab175
Unify <unify@eleccoin.org>
2022-10-29 20:09:56 +0800

    Improve forms of qt

--- src/qt/android/src/org/eleccoincore/qt/EleccoinQtActivity.java
+++ src/qt/android/src/org/eleccoincore/qt/EleccoinQtActivity.java
@@ -18,12 +18,6 @@ public class EleccoinQtActivity extends QtActivity
             eleccoinDir.mkdir();
         }
 
-        try {
-            Os.setenv("QT_QPA_PLATFORM", "android", true);
-        } catch (ErrnoException e) {
-            e.printStackTrace();
-        }
-
         super.onCreate(savedInstanceState);
     }
 }
